27d172bb865c0ac47297b8f10cf9dcb114e0dd64
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-01-16 13:26:20 +0100

    Change * to name

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 
-SELECT * FROM animals WHERE neutered = true AND escape_attempts<3;
+SELECT name FROM animals WHERE neutered = true AND escape_attempts<3;
 
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 
