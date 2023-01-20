ac25117d8c84f9c5f5b16c29a39cd9d5d39e1ad0
PromzzyKoncepts <okechukwupromise638@gmail.com>
2023-01-20 12:23:27 +0100

    Update file with new entries

--- schema.sql
+++ schema.sql
@@ -38,3 +38,31 @@ Add column owner_id which is a foreign key referencing the owner table
 ALTER TABLE animal DROP COLUMN specie;
 ALTER TABLE animal ADD COLUMN specie_id INT REFERENCES specie (id);
 ALTER TABLE animal ADD COLUMN owner_id INT REFERENCES owner(id);
+
+
+-- DAY 4
+
+
+/*Create a table named vets with the following columns:
+id: integer (set it as autoincremented PRIMARY KEY)
+name: string
+age: integer
+date_of_graduation: date
+*/
+CREATE TABLE vet(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(50), age INT, date_of_graduation DATE, PRIMARY KEY(id));
+
+/*
+There is a many-to-many relationship between the tables species and vets:
+a vet can specialize in multiple species,
+and a species can have multiple vets specialized in it.
+Create a "join table" called specializations to handle this relationship.
+*/
+CREATE TABLE specialization(vet_id INT REFERENCES vet (id), specie_id INT REFERENCES specie (id));
+
+/*
+There is a many-to-many relationship between the tables animals and vets:
+an animal can visit multiple vets
+and one vet can be visited by multiple animals.
+Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit. */
+
+CREATE TABLE visits(vet_id INT REFERENCES vet(id), animal_id INT REFERENCES animal (id), visit_date DATE );
